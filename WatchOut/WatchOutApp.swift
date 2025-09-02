6fe5e3d45fb581815eefda61a0497bf5e408e7e8
JaeSunEo <wotjs020708@gmail.com>
2025-09-02 23:32:48 +0900

    initialize

---
+++ WatchOut/WatchOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchOutApp.swift
+//  WatchOut
+//
+//  Created by 어재선 on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct WatchOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
