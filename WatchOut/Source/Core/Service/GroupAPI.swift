51593cfe34c9f56b6e1896c0e294480b6221937f
JaeSunEo <wotjs020708@gmail.com>
2025-09-16 19:35:13 +0900

    feat: GroupAPI 추가

--- WatchOut/Source/Core/Service/GroupAPI.swift
+++ WatchOut/Source/Core/Service/GroupAPI.swift
@@ -6,3 +6,56 @@
 //
 
 import Foundation
+import Moya
+
+enum GroupAPI{
+    case createGroup(CreateGorupRequest)
+    case joinGroup(JoinGorupRequest)
+//    case ManageMentGroup
+//    case WaitingGroup
+}
+
+extension GroupAPI: TargetType{
+    var baseURL: URL {
+        return URL(string: "https://wiheome.ajb.kr/api/family_group")!
+    }
+    
+    var path: String {
+        switch self {
+            
+        case .createGroup:
+            return "/create"
+        case .joinGroup:
+            return "/join"
+//        case .ManageMentGroup:
+//             
+//        case .WaitingGroup:
+//
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+            
+        case .createGroup:
+            return .post
+        case .joinGroup:
+            return .post
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .createGroup(let request):
+            return .requestJSONEncodable(request)
+        case .joinGroup(let request):
+            return .requestJSONEncodable(request)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return ["Content-Type" : "application/json"]
+    }
+    
+    
+}
