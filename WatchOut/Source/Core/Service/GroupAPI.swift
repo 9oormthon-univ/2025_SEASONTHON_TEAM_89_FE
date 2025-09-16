cb2129a19b1386385761596ae79e738d30b0156d
JaeSunEo <wotjs020708@gmail.com>
2025-09-16 19:35:01 +0900

    feat: Log 파일 추가

---
+++ WatchOut/Source/Core/Service/GroupAPI.swift
@@ -0,0 +1,8 @@
+//
+//  GroupAPI.swift
+//  WatchOut
+//
+//  Created by 어재선 on 9/16/25.
+//
+
+import Foundation
---
+++ WatchOut/Source/Core/Service/GroupService.swift
@@ -0,0 +1,8 @@
+//
+//  GroupService.swift
+//  WatchOut
+//
+//  Created by 어재선 on 9/16/25.
+//
+
+import Foundation
