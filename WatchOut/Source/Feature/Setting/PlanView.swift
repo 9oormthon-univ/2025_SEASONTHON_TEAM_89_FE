b0705f2aad63baaaf2b0fe67bb2cb104dc36d5e3
JaeSunEo <wotjs020708@gmail.com>
2025-11-08 23:03:40 +0900

    add: 아이콘, PlanView, LoadingView

---
+++ WatchOut/Source/Feature/Setting/PlanView.swift
@@ -0,0 +1,18 @@
+//
+//  PlanView.swift
+//  WatchOut
+//
+//  Created by 어재선 on 11/8/25.
+//
+
+import SwiftUI
+
+struct PlanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlanView()
+}
---
+++ WatchOut/Source/Feature/Wating/loadingView.swift
@@ -0,0 +1,8 @@
+//
+//  loadingView.swift
+//  WatchOut
+//
+//  Created by 어재선 on 11/8/25.
+//
+
+import Foundation
